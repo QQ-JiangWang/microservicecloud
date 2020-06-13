6195928da642c3092ff00fc9f7ae2f61a86bc86c
QQ-JiangWang <1120454016@qq.com>
2020-06-13 15:58:32 +0800

    新增springcloud基础项目

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/longrise/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,31 @@
+package com.longrise.springcloud.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.longrise.springcloud.dao.DeptDao;
+import com.longrise.springcloud.entities.Dept;
+import com.longrise.springcloud.service.DeptService;
+
+@Service
+public class DeptServiceImpl implements DeptService{
+	@Autowired
+	private DeptDao dao;
+	@Override
+	public boolean add(Dept dept) {
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id) {
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list() {
+		return dao.findAll();
+	}
+	
+}
